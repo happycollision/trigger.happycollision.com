c51af629b7b5d02aa1d751dadbc4f47a8e009221
Don Denton <don@happycollision.com>
2023-03-11 01:35:15 -0500

    Fix config values

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from "astro/config"
 import AstroPWA from "@vite-pwa/astro"
-
-// https://astro.build/config
 import mdx from "@astrojs/mdx"
 import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://happycollision.com",
+  site: "https://trigger.happycollision.com",
   trailingSlash: "always",
   integrations: [
     mdx(),
@@ -15,12 +13,12 @@ export default defineConfig({
     AstroPWA({
       registerType: "autoUpdate",
       manifest: {
-        name: "Happy Collision", // Change this to your website's name.
-        short_name: "Happy Collision", // Change this to your website's short name.
-        description: "The not-really-a-blog for Happy Collision.", // Change this to your websites description.
+        name: "Finger on the Trigger",
+        short_name: "FoTT",
+        description: "Finger on the Trigger band info", // Change this to your websites description.
         theme_color: "#df993a", // Change this to your primary color.
         background_color: "#ffffff", // Change this to your background color.
-        display: "minimal-ui",
+        display: "standalone",
         icons: [
           {
             src: "/favicon.svg",
